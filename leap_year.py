f996cca99a673ae4e2cf8e34cecaa0c4f5625f01
Quimey5 <162723087+Quimey5@users.noreply.github.com>
2024-04-18 23:55:12 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,3 +1,6 @@
 def leap_year():
-    import math
-    print("TO DO")
+    a=int(input("Ingrese un año: "))
+if(a%4==0 and a% 100==0 and a%400!=0) or (a % 4!=0):
+    print("el año",a,"no es bisiesto")
+else:
+    print("elaño",a,"es bisiesto")
